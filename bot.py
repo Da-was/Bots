37df887f959a6a75f57e78fede1d86ff2fe3d2ce
João laurindo <joaoviniciuslaurindo@live.com>
2020-09-01 17:43:40 -0300

    Arrumando sistema de pastas

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import pyautogui,time
 import textos
 #aviso,cirilo,pneu, santana e vanessa
 braba = input('qual vai ser a braba?')
-f = open(braba + '.txt','r')
+f = open('textos/'+braba + '.txt','r')
 time.sleep(4)
 for word in f:
     pyautogui.typewrite(word)
