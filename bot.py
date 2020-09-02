3699bab0bc215b6e0ae7f6edd11a4b2c163c0afb
João laurindo <joaoviniciuslaurindo@live.com>
2020-09-01 23:10:41 -0300

    Colocando mais instruções e botões

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import pyautogui,time
+from PIL import ImageTk, Image
 from tkinter import*
 
 root = Tk()
@@ -21,12 +22,16 @@ def init_spam():
 
 Frame = LabelFrame(root, text= "Como usar:", padx = 5, pady = 5 )
 Frame2 = LabelFrame(root, text= "Definições:", padx = 5, pady = 5 ,)
+Frame3 = LabelFrame(root, text= "Créditos:", padx = 5, pady = 5 ,)
 
-Explain = Label(Frame,text = "Esse é um bot feito para espamar mensagens, tudo o que você precisa fazer \n"
-+" é informar qual das mensagens você quer que seja espamada e clicar no \'iniciar\'.\n\n"
-+"ALERTA : É aconselhado que você já esteja com o lugar que você quer espamar aberto, pois tera apenas \n"
-+ " 5 segundos antes do spam acontecer após clicar em inicar.")
+Explain1 = Label(Frame,text = "Esse é um bot feito para espamar mensagens, tudo o que você precisa fazer \n"
++" é informar qual das mensagens você quer que seja espamada e clicar no \'iniciar\'.")
 
+Explain2 = Label(Frame,text ="ALERTA : É aconselhado que você já esteja com o lugar que você quer espamar\n"
++ " aberto, pois tera apenas 5 segundos antes do spam acontecer após clicar em inicar.")
+
+Explain3 = Label(Frame,text ="O botão \'Abortar\' serve para parar o bot antes que o documento de texto acabe.\n"
++"Ele fecha a janela do bot, então para poder abrir novamente é necessário reabrir o bot.")
 
 
 Select_text= Label(Frame2,text = "Insira o nome do arquivo de texto que você deseja espamar. ")
@@ -34,16 +39,24 @@ Select = Entry(Frame2)
 
 
 Button_init = Button(Frame2, text = "Iniciar", fg = "red", padx = 30 ,command = init_spam)
+button_quit = Button(Frame2, text= "Abortar", padx = 30 , command = root.quit, fg = "red")
 
+status = Label(Frame3, text= "Criado por: João Laurindo", )
 
 #-------------------------------atatch side----------------------------
-Explain.grid(row =0 , column = 0)
+Explain1.grid(row =0 , column = 0, padx = 80)
+Explain2.grid(row =1 , column = 0)
+Explain3.grid(row =2 , column = 0)
 
 Select_text.grid(row = 0 , column = 0)
 Select.grid(row =1 , column = 0)
-Button_init.grid(row = 1, column = 1, columnspan = 2, padx = 63)
+Button_init.grid(row = 1, column = 1, padx = 9)
+button_quit.grid(row = 1, column = 2, )
 
-Frame.grid(row =0 , column = 0, padx = 10, pady = 10, sticky = W)
-Frame2.grid(row =1 , column = 0, padx = 10, pady = 10, sticky = W)
+status.grid(row = 0 , column = 0)
+
+Frame.grid(row =0 , column = 0, padx = 10, pady = 10, sticky = W+E)
+Frame2.grid(row =1 , column = 0, padx = 10, pady = 10, sticky = W+E)
+Frame3.grid(row =2 , column = 0, padx = 10, pady = 10, sticky = W+E)
 
 root.mainloop()
